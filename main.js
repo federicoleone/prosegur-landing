22f2b9c01025d292be9d942b816d70575ce42afb
fleonea <32718106+fleonea@users.noreply.github.com>
2019-02-13 17:59:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,96 @@
+
+$(function(){
+    /* Header add bg black */
+    function nav_scroll_bg(){
+        if ($(window).scrollTop() > 30) {
+           $('.navbar').addClass('nav-landing-scroll-bg');
+        } else {
+           $('.navbar').removeClass('nav-landing-scroll-bg');
+        }       
+    }
+
+	nav_scroll_bg();
+
+	$(window).scroll(function(){
+		nav_scroll_bg();
+	});	
+	
+
+    /* Silder */
+    var nextH;
+    $('.carousel').on('slide.bs.carousel', function (e) {
+        nextH = $(e.relatedTarget).height();
+        $('.carousel-inner').animate({ height: nextH }, 500);
+        setTimeout(function(){
+            $('.carousel-inner').height('');
+        }, 1000);
+    });
+
+    var currentH;
+    $(window).on('resize', function(){
+        currentH = $('.carousel-item.active').height();
+        $('.carousel-inner').height(currentH);
+        setTimeout(function(){
+            $('.carousel-inner').height('');
+        }, 1000);
+    });
+
+
+    /* AOS */
+    AOS.init();
+
+    /* Visible el on screen 
+    $.fn.isInViewport = function() {
+      var elementTop = $(this).offset().top;
+      var elementBottom = elementTop + $(this).outerHeight();
+
+      var viewportTop = $(window).scrollTop();
+      var viewportBottom = viewportTop + $(window).height();
+
+      return elementBottom > viewportTop && elementTop < viewportBottom;
+    };
+
+
+    $(window).on('resize scroll', function() {
+      $('#carousel-mainbanner').each(function() {
+        if ($(this).isInViewport()) {
+            $(this).carousel('cycle');
+        } else {
+            $(this).carousel('pause');
+        }
+      });
+
+      $('#carousel-mainbanner').mouseover(function(){
+            $(this).carousel('pause');     
+      });
+    });
+    */
+
+    $('#carousel-mainbanner').carousel({
+        pause: "false"
+    });
+
+    $('#carousel-mainbanner').find('input').on('click', function(){
+        $('#carousel-mainbanner').carousel('pause')
+    });
+
+});
+
+/* Validation */
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
