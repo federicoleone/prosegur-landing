1d781945e34c9cd3d30d9f5f1a80ef22d31619dc
fleonea <32718106+fleonea@users.noreply.github.com>
2019-02-15 11:08:08 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -39,13 +39,31 @@ $(function(){
     /* AOS */
     AOS.init();
 
+    /* Visible el on screen 
+    $.fn.isInViewport = function() {
+      var elementTop = $(this).offset().top;
+      var elementBottom = elementTop + $(this).outerHeight();
 
-    $('#carousel-mainbanner').carousel({
-        pause: "false"
+      var viewportTop = $(window).scrollTop();
+      var viewportBottom = viewportTop + $(window).height();
+
+      return elementBottom > viewportTop && elementTop < viewportBottom;
+    };
+
+    $(window).on('resize scroll', function() {
+      $('#carousel-mainbanner').each(function() {
+        if ($(this).isInViewport()) {
+            $(this).carousel('cycle');
+        } else {
+            $(this).carousel('pause');
+        }
+      });
     });
 
-    $('#carousel-mainbanner input').on('click', function(){
-        $('#carousel-mainbanner').carousel('pause')
+    */
+
+    $('#carousel-mainbanner').carousel({
+        pause: "hover"
     });
 
 });
